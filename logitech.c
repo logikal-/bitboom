d7682ea616c31050aba101f741f9f69d67f1cedb
John <0x4a6f686e@gmail.com>
2012-05-14 09:53:09 -0400

    added logitech serial generator

---
+++ logitech.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define round(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+
+int main()
+{
+	int  y, r, d = 0;
+	double w;
+	while(!((w>5)&&(w<48)))
+	{
+		srand(time(NULL));
+		w = 1 + (int)( 100.0 * rand() / ( RAND_MAX + 1.0 ) );
+	}
+	while(!((y>6)&&(y<10)))
+	{
+		y = 1 + (int)( 9.0 * rand() / ( RAND_MAX + 1.0 ) );
+	}
+	r = 1 + (int)( 9.0 * rand() / ( RAND_MAX + 1.0 ) );
+	d = 1 + (int) ( 30 * rand() / ( RAND_MAX + 1.0 ) );
+	printf("\nSERIAL: LZ %d %d %d MC\n", (int)w, y, r);
+	printf("\nPURCHASED: m/d/y %.0f/%d/%d\n\n", w/52*12, d, y+2000);
+}
+
+
