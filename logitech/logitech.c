4af7d40dae45278792a19e74b8fa57ce2e716179
John <0x4a6f686e@gmail.com>
2012-05-14 10:14:30 -0400

    cleaner

---
+++ logitech/logitech.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define round(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+
+int main()
+{
+	int  y, r = 0;
+	double w, d;
+	srand(time(NULL));
+	while(!(w>5))
+	{
+		w = 1 + (int)( 48.0 * rand() / ( RAND_MAX + 1.0 ) );
+	}
+	while(!(y>6))
+	{
+		y = 1 + (int)( 9.0 * rand() / ( RAND_MAX + 1.0 ) );
+	}
+	r = 1 + (int)( 9.0 * rand() / ( RAND_MAX + 1.0 ) );
+	d = 1 + (int) ( 30.0 * rand() / ( RAND_MAX + 1.0 ) );
+	printf("\nSERIAL: LZ%d%d%dMC\n", (int)w, y, r);
+	printf("\nPURCHASED: m/d/y %.0f/%d/%d\n\n", w/52*12, (int)d, y+2000);
+}
